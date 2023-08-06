fac804dc1f23a3930280289828db8450ad9d6189
mbhavya23 <125909060+mbhavya23@users.noreply.github.com>
2023-08-06 22:29:42 +0530

    Update home_page.dart

--- Final/lib/pages/home_page.dart
+++ Final/lib/pages/home_page.dart
@@ -1,7 +1,7 @@
-import 'package:assignment3/pages/login_register_page.dart';
+import 'package:Final/pages/login_register_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:assignment3/auth.dart';
+import 'package:Final/auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:path_provider/path_provider.dart';
